0969bb2b9196ead5cdecb7414957db34ded03781
shubham sharma <shubhamtech004@gmail.com>
2024-09-05 17:12:37 +0530

    componet and hooks created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
-
-
+import {InputBox} from './component'
+import usecurrencyInfo from './hooks/useCurrencyInfo'
 function App() {
-  
-
+      
+      const [amount, setamount] = useState(0)
+      const [form , setFrom] = useState("usd")
+      const [to , setTo] = useState("inr")
+      const[converetedammount , setconveryedAmount] = useState(0)
   return (
     <>
     <h1>yes react</h1>
